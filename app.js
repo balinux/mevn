fa621f4b959e1561949084367c706b90b2eaa45f
Glitch (mevn) <none>
2018-05-31 02:17:38 +0000

    🙉👰 Checkpoint ./app.js:1028656/195 ./package.json:1028656/1386

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 const express  = require('express');
 const app = express();
 const morgan = require('morgan');
-
+const bodyParser = require('body-parser');
 
 const orderRouter = require('./api/routes/orders');
 
+app.use(bodyParser.urlencoded({extended:false}));
+app.use(bodyParser.json());
 app.use(morgan('dev'));
 app.use('/order', orderRouter)
 
