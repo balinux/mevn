0b39543deeeec254cdbdc70574803b26af2c4c0f
Glitch (mevn) <none>
2018-05-31 01:05:44 +0000

    👨‍👨‍👦🐔 Checkpoint ./app.js:1028656/234 ./server.js:1028656/197

--- server.js
+++ server.js
@@ -2,13 +2,15 @@
 // where your node app starts
 
 // init project
-var express = require('express');
-var app = express();
+var http = require('http');
+var app = require('./app');
 
-app.use(express.static('public'));
-app.get("/", function (request, response) {
-  response.sendFile(__dirname + '/views/index.html');
-});
+// app.use(express.static('public'));
+// app.get("/", function (request, response) {
+//   response.sendFile(__dirname + '/views/index.html');
+// });
+
+const server = http.CreateServer(app);
 
 
 var listener = app.listen(process.env.PORT, function () {
