5a83dae9d981c0a70895d9575d9171a496d28bc5
Glitch (mevn) <none>
2018-05-31 02:06:53 +0000

    👴🙄 Checkpoint ./server.js:1028656/6 ./app.js:1028656/686

--- app.js
+++ app.js
@@ -1,9 +1,26 @@
 const express  = require('express');
 const app = express();
+const morgan = require('morgan');
+
 
 const orderRouter = require('./api/routes/orders');
 
-
+app.use(morgan('dev'));
 app.use('/order', orderRouter)
+
+app.use((req,res, next) => {
+  const error = new Error('Not Found!')
+  error.status(404);
+  next(error);
+})
+
+app.use((error, req, res, next) => {
+  res.status(error.status || 500);
+  res.json({
+    error: {
+      message:error.message
+    }
+  })
+})
 
 module.exports = app;
